feee47c6991f40762fa9e80d546f5d3f882ddf9f
Vongsapat Bandhuprabhasa <vongsapat.ban@outlook.com>
2022-10-27 02:28:28 +0700

    MAKE /category/[CATEGORY_NAME]

--- pages/category/[cat].jsx
+++ pages/category/[cat].jsx
@@ -1,25 +1,26 @@
 /** @jsxImportSource @emotion/react */
 
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import { css } from '@emotion/react'
 import Link from 'next/link'
 
 import colors from 'constants/colors.js'
 import fonts from 'constants/fonts.js'
-
 import categories from 'constants/categories.json'
+import games from 'constants/games.json'
+
 import NavBar from 'components/common/NavBar'
+import GameCard from 'components/common/GameCard'
 
 
 function Category({ }) {
   return (
     <>
       <NavBar hasLogo={false} />
-
       <div css={css`
         display: flex;
         min-height: 1000px;
-
       `}>
         <CategoryOptions />
         <ResultList />
@@ -51,7 +52,11 @@ function CategoryOptions() {
         gap: 8px 4px;
       `}>
         {
-          categories.filter(cat => cat.showOnCategory).map((cat) => <CategoryButton key={cat.name + cat.id} {...cat} >{cat.en_displayTag}</CategoryButton>) // {...cat } = unpack keys and values into CategoryButton
+          categories.filter(cat => cat.showOnCategory).map((cat) => (
+            <CategoryButton key={cat.name + cat.id} {...cat} >
+              {cat.en_displayTag}
+            </CategoryButton>
+          )) // {...cat } = unpack keys and values into CategoryButton
         }
       </div>
     </div>
@@ -59,27 +64,13 @@ function CategoryOptions() {
 }
 
 function CategoryButton(props) {
-  const router = useRouter()
-  const catQuery = router.query
   return (
-    <Link href={'/category' + props.link}>
+    <Link href={'/category/' + props.link}>
       <a css={css`
-
         font-family: ${fonts.normalFontFamily};
         font-size: 16px;
-        ${
-          catQuery.cat == props.link ?
-            `
-            color: ${colors.white};
-            box-shadow: 0px 0px 4px ${colors.white};
-            background-color: ${colors.greenPrimary};
-            `
-          :
-            `
-            color: ${colors.black};
-            background-color: ${colors.white};
-            `
-        };
+        color: ${colors.black};
+        background-color: ${colors.white};
         padding: 3px 5px;
         border-radius: 10000px;
         cursor: pointer;
@@ -91,14 +82,50 @@ function CategoryButton(props) {
 }
 
 function ResultList() {
+  const router = useRouter()
+  const [category, setCat] = useState({})
+  const [gameCards, setGamesCards] = useState(<div />)
+
+  useEffect(function () {
+    if (!router.isReady) return
+    setCat(() =>
+      categories.find(i =>
+        i.link == router.query.cat
+      )
+    )
+  }, [router.isReady, router.query.cat])
+  useEffect(function() {
+    setGamesCards(() =>
+      games
+        .filter(game => game.tags.includes(category.id))
+        .map(gameInfo => <GameCard{...gameInfo} />)
+    )
+  }, [category])
+
   return (
-    <div css={css`
+    <main css={css`
       background-color: ${colors.white};
       flex: 1;
+      padding: 10px 40px;
     `}>
-
-    </div>
+      <span css={css`
+        font-size: 32px;
+        font-family: ${fonts.normalFontFamily};
+        font-weight: 800;
+      `}>
+        {category.en_displayTag}
+      </span>
+      <div css={css`
+        display: grid;
+        grid-template-columns: auto auto auto auto auto auto auto;
+        gap: 15px 10px;
+      `}>
+        { gameCards }
+      </div>
+    </main>
   )
 }
+
+
 
 export default Category
