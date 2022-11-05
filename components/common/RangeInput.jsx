2d5c3213e1e3758fec9b9ee41e95aaa2d06b2f8f
Vongsapat Bandhuprabhasa <vongsapat.ban@outlook.com>
2022-11-06 02:47:56 +0700

    todo: RangeInput

---
+++ components/common/RangeInput.jsx
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */
+
+import React, { useState, useEffect, useRef, forwardRef } from 'react'
+import { css } from '@emotion/react'
+
+function RangeInput(props) {
+  //todo range input
+  const { minMax } = props
+  return (
+    <div css={css`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    `}>
+      <div css={css`
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        gap: 5px;
+      `}>
+        <div css={css`all: unset; flex: 1; background-color: white;`} />
+        <span css={css`margin: 0 10px;`}>-</span>
+        <div css={css`all: unset; flex: 1; background-color: white;`} />
+      </div>
+    </div>
+  )
+}
+
+export default RangeInput
