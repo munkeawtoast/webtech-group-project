da9e58df99bba61abbf1356cac8cbb18b7b23cd7
Vongsapat Bandhuprabhasa <vongsapat.ban@outlook.com>
2022-11-05 19:03:43 +0700

    ADD redirect '/' to '/category' REMOVE index.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/category',
+        permanent: true
+      }
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
