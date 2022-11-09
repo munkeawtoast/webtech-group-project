9691f5a0285dbff57560ad1ac074c88ef1501d27
KayKaedesu <88417199+KayKaedesu@users.noreply.github.com>
2022-11-09 22:06:41 +0700

    Build more game page

---
+++ components/game/GameImages.jsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import { css } from "@emotion/react";
+const GameImages = () => {
+  return <div></div>;
+};
+
+export default GameImages;
--- pages/game/[game].jsx
+++ pages/game/[game].jsx
@@ -2,12 +2,23 @@ import NavBar from "components/common/NavBar";
 import { css } from "@emotion/react";
 import GameInfo from "components/game/GameInfo";
 import Footer from "components/common/Footer";
+import TopImage from "components/game/GameTopImage";
 
 const Game = () => {
   return (
     <div>
       <NavBar />
-      <GameInfo />
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 36px;
+        `}
+      >
+        <TopImage />
+        <GameInfo />
+      </div>
       <Footer />
     </div>
   );
