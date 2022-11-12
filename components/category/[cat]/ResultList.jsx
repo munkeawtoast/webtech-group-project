5b34f5a9f8e1bf0a8875226564a895d72578a716
Vongsapat Bandhuprabhasa <vongsapat.ban@outlook.com>
2022-11-12 20:41:09 +0700

    tuned padding a little bit

--- components/category/[cat]/ResultList.jsx
+++ components/category/[cat]/ResultList.jsx
@@ -1,11 +1,33 @@
-import { css } from "@emotion/react";
+import { useEffect, useState } from "react"
+import { css } from "@emotion/react"
 
-import GameCard from "components/common/GameCard";
-import colors from "constants/colors.js";
-import fonts from "constants/fonts.js";
-import { mediaQueries as mq } from "constants/mediaqueries";
+import GameCard from "components/common/GameCard"
+import colors from "constants/colors.js"
+import fonts from "constants/fonts.js"
+import { mediaQueries as mq } from "constants/mediaqueries"
 
 export default function ResultList({ sortFunc, userPriceRange, category, games }) {
+  const [gameComps, setGameComps] = useState([])
+
+  useEffect(() => {
+    setGameComps(games
+      .filter(game => game.price['thb'] <= userPriceRange[1] && game.price['thb'] >= userPriceRange[0])
+      .sort(sortFunc.func('thb'))
+      .map(game => (
+        <GameCard
+          key={game.name}
+          game={game}
+          showArgs={{
+            showImage: true,
+            showName: true,
+            showButton: true,
+            isLink: true,
+          }}
+        />
+      ))
+    )
+  }, [games, sortFunc, userPriceRange])
+
   return (
     <main
       css={css`
@@ -36,29 +58,27 @@ export default function ResultList({ sortFunc, userPriceRange, category, games }
           display: flex;
           flex-wrap: wrap;
           gap: 20px;
+          ${mq[1]} {
+            margin: 0 15px;
+          }
           ${mq[2]} {
             justify-content: center;
           }
         `}
       >
-        {games
-          .filter(game => {
-            console.log(userPriceRange[0], game.price['thb'], userPriceRange[1])
-            return (game.price['thb'] <= userPriceRange[1] && game.price['thb'] >= userPriceRange[0])
-          })
-          .sort(sortFunc.func('thb'))
-          .map((game) => (
-            <GameCard
-              key={game.name}
-              game={game}
-              showArgs={{
-                showImage: true,
-                showName: true,
-                showButton: true,
-                isLink: true,
-              }}
-            />
-          ))
+        {
+          gameComps.length !== 0
+            ? gameComps
+            :
+            <span css={
+              css`
+                color: ${colors.gray500};
+                font-size: 1.75em;
+                ${mq[1]} {
+                  padding-left: 10px;
+                }
+              `
+            }>No games found 😢</span>
         }
       </div>
     </main>
