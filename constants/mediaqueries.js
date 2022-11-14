ab954943e56c63e205d9795615a9bca8c5c2a364
Vongsapat Bandhuprabhasa <vongsapat.ban@outlook.com>
2022-11-14 18:39:04 +0700

    mq[0] from 1024 to 1240

--- constants/mediaqueries.js
+++ constants/mediaqueries.js
@@ -1,2 +1,2 @@
-const mediaQueriesSizes = [1024, 768, 480] 
+const mediaQueriesSizes = [1240, 768, 480] 
 export const mediaQueries = mediaQueriesSizes.map(s => `@media screen and (max-width: ${s}px)`)
